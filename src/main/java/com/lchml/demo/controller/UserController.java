95333ef1e57596fcc5e216c28400200ab5512777
licheng_xd <licheng_xd@163.com>
2024-05-17 10:48:23 +0800

    code

--- src/main/java/com/lchml/demo/controller/UserController.java
+++ src/main/java/com/lchml/demo/controller/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
-import java.util.Date;
 import java.util.Objects;
 
 @Controller
